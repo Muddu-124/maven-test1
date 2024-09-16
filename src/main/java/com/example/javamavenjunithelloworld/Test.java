7c76b27616ceb2d96fd0d11f62451418a57d7a3e
KRISHNA MURTHY <koppula.2076@gmail.com>
2024-09-16 13:20:48 +0530

    https://github.com/Muddu-124/maven-test1/tree/KRISHNAL0001/src

---
+++ src/main/java/com/example/javamavenjunithelloworld/Test.java
@@ -0,0 +1,11 @@
+package com.example.javamavenjunithelloworld;
+public class Test {
+    public static void main(String[] args) {
+        int n = 10; // Initialize n with a value (e.g., 10)
+        if (n % 2 == 0) {
+            System.out.println(n + " is an even number");
+        } else {
+            System.out.println(n + " is an odd number");
+        }
+    }
+}
